eeef040354da573d8915a7493de1f13e696b73f6
ValehK08 <valeh.kazimli@outlook.com>
2025-12-19 17:24:07 +0400

    .

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """LOG STATS"""
-
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client.logs
-col = db['nginx']
-doc_counts = col.count_documents({})
-print(f"{doc_counts} logs")
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print("Methods:")
-for meth in method:
-    print(f"    method {meth}: {col.count_documents({'method': meth})}")
-count = col.count_documents({'method': "GET", "path": "/status"})
-print(f"{count} status check")
+def log_stats():
+    client = MongoClient()
+    db = client.logs
+    col = db['nginx']
+    doc_counts = col.count_documents({})
+    print(f"{doc_counts} logs")
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for meth in method:
+        print(f"    method {meth}: {col.count_documents({'method': meth})}")
+    count = col.count_documents({'method': "GET", "path": "/status"})
+    print(f"{count} status check")
+
+if __name__ = "__main__":
+    log_stats()
