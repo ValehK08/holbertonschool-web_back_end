55b554ae253f469367dfab5849d6f40a3b85f544
ValehK08 <valeh.kazimli@outlook.com>
2025-12-19 16:59:43 +0400

    .

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,4 +4,4 @@
 def update_topics(mongo_collection, name, topics):
     """FUNCTION TO UPDATE TOPICS"""
     
-    return mongo_collection.update_many({'name': name}, {$set: {'topics': topics}})
+    return mongo_collection.update_many({'name': name}, {"$set": {'topics': topics}})
